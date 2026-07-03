cfde18c3be4c9665477f0889d2a3b2024a60d694
Viktor Smirnov <tin.r@mail.ru>
2026-07-03 15:26:42 +0300

    update mask_account_card function

--- src/widget.py
+++ src/widget.py
@@ -6,19 +6,28 @@ def mask_account_card(account_type_and_number: str) -> Union[str, None]:
     """Маскировка информации о счетах и картах в строке"""
 
     substrings = account_type_and_number.split()
-    if not 2 <= len(substrings) <= 3:
+    if len(substrings) < 2: # Проверяем не пусты ли подстроки с типом и номером счета
         return None
 
-    if substrings[0].lower() == "счет" or substrings[0].lower() == "счёт":
-        if not substrings[-1].isdigit() or len(substrings[-1]) < 4:
-            return None
-        substrings[-1] = masks.get_mask_account(substrings[-1])
-    else:
-        if not substrings[0].isalpha() or not substrings[-1].isdigit() or not substrings[-2].isalpha() or len(substrings[-1]) != 16:
-            return None
-        substrings[-1] = masks.get_mask_card_number(substrings[-1])
+    account_number = substrings[-1]
+    if not account_number.isdigit() or len(account_number) < 4: # Проверяем корректность данных номера счета
+        return None
 
-    return " ".join(substrings)
+    # Собираем название типа счета, если в нем были символы пробела
+    account_type = ""
+    for i in range(len(substrings) - 1):
+        if substrings[i].isalpha(): # в типе счета не должно быть цифр
+            account_type += substrings[i] + " "
+        else:
+            return None
+
+    # Маскировка в зависимости от типа счета
+    if account_type[:4].lower() == "счет" or account_type[:4].lower() == "счёт":
+        return account_type + masks.get_mask_account(account_number)
+    else:
+        if len(account_number) != 16:
+            return None
+        return account_type + masks.get_mask_card_number(account_number)
 
 
 def get_date(date: str) -> Union[str, None]:
@@ -39,3 +48,5 @@ if __name__ == "__main__":
     print(mask_account_card("Счет 73654108430135874305"))
 
     print(get_date("2024-03-11T02:26:18.671407"))
+
+    print(mask_account_card("Maestro y 596837868705199"))
