85bf3745f87188515e0e559980d8ea90c0234cde
Viktor Smirnov <tin.r@mail.ru>
2026-07-10 14:03:35 +0300

    added filter_by_state function

--- src/processing.py
+++ src/processing.py
@@ -1,1 +1,28 @@
-# Будет содержать новые функции обработки данных.
+def filter_by_state(list_of_dict: list, state: str = "EXECUTED") -> list:
+    """Возвращает новый список, содержащий только те словари, у которых ключ state соответствует."""
+    return [item for item in list_of_dict if item["state"] == state]
+
+
+if __name__ == "__main__":
+    # Проверка работы функции filter_by_state
+    print(
+        filter_by_state(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+    )
+    print(
+        filter_by_state(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "CANCELED",
+        )
+    )
