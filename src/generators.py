e131f49cab3faa6ff7590c8eb3fc0cdd51c730d2
Viktor Smirnov <tin.r@mail.ru>
2026-07-23 14:41:57 +0300

    Added transaction_descriptions function to generators.py module

--- src/generators.py
+++ src/generators.py
@@ -3,3 +3,8 @@ from typing import Iterator
 
 def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
     return filter(lambda transaction: transaction["operationAmount"]["currency"]["code"] == currency, transactions)
+
+
+def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
+    for transaction in transactions:
+        yield transaction["description"]
