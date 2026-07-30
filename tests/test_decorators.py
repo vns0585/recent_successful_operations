b92f8e49171a4ec063a44e9513d8f44e9c98e7bc
Viktor Smirnov <tin.r@mail.ru>
2026-07-30 16:02:04 +0300

    fixed bugs

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -44,3 +44,11 @@ def test_log_error_file() -> None:
     with open("mylog.txt", "r", encoding="utf-8") as file:
         assert file.read() == "my_function error: Тестовая ошибка. Inputs: (1, 2), {}\n"
     remove("mylog.txt")
+
+
+def test_log_wrong_filename() -> None:
+    @log(1)
+    def my_function(x: int, y: int) -> int:
+        return x + y
+    with pytest.raises(Exception, match="Имя файла должно быть строкового типа"):
+        my_function(1, 2)
